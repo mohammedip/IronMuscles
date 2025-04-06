af4758097d9bdd0ebaf90900b44a719f578699d0
Mohammed reida <reidamohammed3@gmail.com>
2025-04-06 18:51:24 +0100

    create the store and the update request rull

---
+++ app/Http/Requests/StoreCoachRequest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreCoachRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'id_specialite' => 'required|string|max:255',
+            'numero_telephone' => 'required|string|max:20',
+            'email' => 'required|email|unique:coaches,email',
+            'date_recrutement' => 'required|date|before_or_equal:' . Carbon::now()->toDateString(),
+            'password' => 'required|string|min:6',
+
+        ];
+    }
+}
