16ac6238d32ed4e2437d73303e50c0de955421bf
Mohammed reida <reidamohammed3@gmail.com>
2025-04-13 19:16:00 +0100

    installe the img and qr code packeges

---
+++ config/image.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Image Driver
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which image library you want to use to manipulate images.
+    | You can choose between the GD library and ImageMagick. By default, we use
+    | the GD library, but you can switch to ImageMagick if you prefer.
+    |
+    */
+
+    'driver' => 'gd', 
+];
