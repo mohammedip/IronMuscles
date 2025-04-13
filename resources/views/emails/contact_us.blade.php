0c5a8a3a7c4cc23aff473d9eca62049b431b9c2b
Mohammed reida <reidamohammed3@gmail.com>
2025-04-13 19:23:34 +0100

    create the blade page for the contact us , NewsLetter and welcome emails

---
+++ resources/views/emails/contact_us.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>New Contact Us Message</title>
+</head>
+<body>
+    <h1>You Have a New Message from {{ $name }}, ({{ $email }})</h1>
+    <p><strong>Message:</strong></p>
+    <p>{{ $msg }}</p>
+</body>
+</html>
